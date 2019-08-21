53cdbeb20d84c16e57b8d44b05ac037b8ab34afb
Trahan.hunter <Trahan.hunter@yahoo.com>
2019-08-21 09:51:08 -0500

    Implemented basic combat

---
+++ SimpleRPG/Game.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRPG
+{
+    class Game
+    {
+        public void Start()
+        {
+            string playerName = "";
+            int playerHealth = 100;
+
+            //Welcome the player
+            Console.Write("What is your name? ");
+            playerName = Console.ReadLine();
+            Console.WriteLine("Welcome, " + playerName + ".");
+
+            //Monster encounter!
+            int monsterDamage = 13;
+            Console.WriteLine("");
+            Console.WriteLine("A monster approaches!");
+
+            //Player actions
+            string action = "";
+            Console.Write("What will you do? (Fight/Flee) ");
+            action = Console.ReadLine();
+
+            if (action == "Fight" || action == "fight")
+            {
+                //monster attack
+                Console.WriteLine("The monster attacks " + playerName + " takes " + monsterDamage + " damage!");
+                playerHealth = playerHealth - monsterDamage;
+                Console.WriteLine(playerName + " Has " + playerHealth + " points of health remaining.");
+
+                //player attack
+                Console.WriteLine(playerName + " attacks! The monster is defeated!");
+            }
+
+            else if (action == "Flee" || action == "flee")
+            {
+                //escape
+                Console.WriteLine(playerName + " got away safely...");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- SimpleRPG/Program.cs
+++ SimpleRPG/Program.cs
@@ -10,6 +10,8 @@ namespace SimpleRPG
     {
         static void Main(string[] args)
         {
+            Game game = new Game();
+            game.Start();
         }
     }
 }
